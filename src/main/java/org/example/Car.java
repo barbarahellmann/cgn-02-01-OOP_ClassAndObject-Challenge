56eb1df6780d6999ea0edeaae14ca3ca5cc3a0fb
barbarahellmann <babshellmann@web.de>
2024-03-13 17:25:46 +0100

    Added input values for cars

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -4,11 +4,20 @@ public class Car {
     String brand;
     String model;
     String color;
-    int ManuYear;
+    String manuYear;
     int Speed = 50;
 
     public void starts(){
-        System.out.println("BrumBrum");
+
+        System.out.println("The " + brand + model + " from " + manuYear + " is " + color + ".");
+    }
+
+    public Car(String brand, String model, String color, String manuYear, int speed) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.manuYear = manuYear;
+        Speed = speed;
     }
 
     public void goFaster(){
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Car usedCar = new Car();
-        Car lamborghiniCar = new Car();
+        Car usedCar = new Car("BMW","3","red","2023", 50);
+        Car lamborghiniCar = new Car("Lamborghini","Y","black","2022", 100);
         usedCar.starts();
+
+
         System.out.println(lamborghiniCar.Speed);
         lamborghiniCar.goFaster();
         System.out.println(lamborghiniCar.Speed);
         System.out.println(usedCar.Speed);
+
+
 
         Person Christian = new Person("Christian", 25, "male");
         Person Dean = new Person("Dean", 29, "male");

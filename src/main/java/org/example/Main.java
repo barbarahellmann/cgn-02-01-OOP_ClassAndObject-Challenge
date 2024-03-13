696259ea59b5fc8ca8c63d5223ffa70a23579fe2
barbarahellmann <babshellmann@web.de>
2024-03-13 16:47:57 +0100

    Added Bonus task "Person"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,12 @@ public class Main {
         lamborghiniCar.goFaster();
         System.out.println(lamborghiniCar.Speed);
         System.out.println(usedCar.Speed);
+
+        Person Christian = new Person("Christian", 25, "male");
+        Person Dean = new Person("Dean", 29, "male");
+        Person Dominik = new Person("Dominik", 33, "male");
+        System.out.println("Hello, I am " + Christian.name + " and " + Christian.age + " years old.");
+        System.out.println("Hello, I am " + Dean.name + " and I´m " + Dean.gender + ".");
+        System.out.println("Hello, I am " + Dominik.name + " and " + Dominik.age + " years old.");
     }
 }

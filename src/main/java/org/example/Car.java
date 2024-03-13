4dad7bbe4e8ecf2aa03b81317fc62b831beb85ea
barbarahellmann <babshellmann@web.de>
2024-03-13 16:29:21 +0100

    Give out Speed for both cars

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,5 +1,18 @@
 package org.example;
 
 public class Car {
-    
+    String brand;
+    String model;
+    String color;
+    int ManuYear;
+    int Speed = 50;
+
+    public void starts(){
+        System.out.println("BrumBrum");
+    }
+
+    public void goFaster(){
+        this.Speed++;
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car usedCar = new Car();
+        Car lamborghiniCar = new Car();
+        usedCar.starts();
+        System.out.println(lamborghiniCar.Speed);
+        lamborghiniCar.goFaster();
+        System.out.println(lamborghiniCar.Speed);
+        System.out.println(usedCar.Speed);
     }
 }
